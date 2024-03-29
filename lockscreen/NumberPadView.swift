6ee2ebd345be38f525d52de0e8aca4b3fee9e3e3
ehsan2793 <ehsan2793@gmail.com>
2024-03-28 22:32:16 -0700

    first commit

---
+++ lockscreen/NumberPadView.swift
@@ -0,0 +1,18 @@
+//
+//  NumberPadView.swift
+//  lockscreen
+//
+//  Created by Ehsan Rahimi on 3/28/24.
+//
+
+import SwiftUI
+
+struct NumberPadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NumberPadView()
+}
---
+++ lockscreen/PasscodeView.swift
@@ -0,0 +1,18 @@
+//
+//  PasscodeView.swift
+//  lockscreen
+//
+//  Created by Ehsan Rahimi on 3/28/24.
+//
+
+import SwiftUI
+
+struct PasscodeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PasscodeView()
+}

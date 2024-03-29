a75033f6016302384fd6f67ed9df9932b7eec4d1
ehsan2793 <ehsan2793@gmail.com>
2024-03-28 21:33:00 -0700

    Initial Commit

---
+++ lockscreen/lockscreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  lockscreenApp.swift
+//  lockscreen
+//
+//  Created by Ehsan Rahimi on 3/28/24.
+//
+
+import SwiftUI
+
+@main
+struct lockscreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
